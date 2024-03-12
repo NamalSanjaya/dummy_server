02e54af19bb6f794c0b8c61af5a223b2242143fc
Namal Sanjaya <namal.sanjaya@axiatadigitallabs.com>
2024-03-12 15:32:19 +0530

    Add agreement resources

--- server.js
+++ server.js
@@ -2,16 +2,71 @@ const express = require('express');
 const app = express();
 const { activeSites, agreementsIds, cities } = require("./data")
 
-/**
- * List Active Sites
- */
+app.use(express.json());
+
+// Dummy dbs
+const agreementDb = []
+const landOwnerDb = []
+const payeeDb = []
+const commTermsDb = []
+
+// Dummy Automatic increment IDs
+let agreementIdNo = 1
+
+// Agreements
+app.get('/agreements/:id', (req, res) => {
+    const agrId = parseInt(req?.params?.id, 10)
+    const agreement = agreementDb.find( agr => agr.AGREEMENT_ID === agrId ) || {}
+    res.send({data: agreement, msg: "Ok"});
+});
+
+app.post('/agreements', (req, res) => {
+    let agr = req.body || {};
+    agr.AGREEMENT_ID = agreementIdNo
+    agreementIdNo++
+    agreementDb.push(agr)
+    res.send({msg: "ok", data: { id: agreementIdNo-1}});
+});
+
+// Land Owner Resource
+app.get('/landOwners/:id', (req, res) => {
+    res.send({data: landOwnerDb[0], msg: "Ok"});
+});
+
+app.post('/landOwners', (req, res) => {
+    const owner = req.body || {};
+    landOwnerDb.push(owner)
+    res.send({msg: "ok", resource: "Land Owner"});
+});
+
+// Payee Resource
+app.get('/payees/:id', (req, res) => {
+    res.send({data: payeeDb[0], msg: "Ok"});
+});
+
+app.post('/payees', (req, res) => {
+    const payee = req.body || {};
+    payeeDb.push(payee)
+    res.send({msg: "ok", resource: "Payee"});
+});
+
+// Commerial Terms Resource
+app.get('/comm-terms/:id', (req, res) => {
+    res.send({data: commTermsDb[0], msg: "Ok"});
+});
+
+app.post('/comm-terms', (req, res) => {
+    const commTerms = req.body || {};
+    commTermsDb.push(commTerms)
+    res.send({msg: "ok", resource: "Commerial Terms"});
+});
+
+// List Active Sites
 app.get('/active-sites', (req, res) => {
     res.send(activeSites);
 });
 
-/**
- * Get agreements for a site id
- */
+// Get agreements for a site id
 app.get('/agreements/:id', (req, res) => {
     // no
     if(req.params.id === "NS001") {
